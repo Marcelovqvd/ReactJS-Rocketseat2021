cbd0e5540dbacd5500b5cefd36770a9bee380691
Marcelovqvd <marcelovqvd@gmail.com>
2021-09-29 15:27:44 -0300

    Props

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,45 +1,21 @@
+import { RepositoryItem } from "./RepositoryItem";
+
+const repository = {
+  name: "unform",
+  description: "Forms in React",
+  link: 'https://github.com/unform/unform'
+}
+
 export function RepositoryList() {
   return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
 
-      <ul>
-        <li>
-          <strong>unfrom</strong>
-          <p>Froms in React</p>
-          <a href="">
-            Acessar Repositórios
-          </a>
-        </li>
-        <li>
-          <strong>unfrom</strong>
-          <p>Froms in React</p>
-          <a href="">
-            Acessar Repositórios
-          </a>
-        </li>
-
-        <li>
-          <strong>unfrom</strong>
-          <p>Froms in React</p>
-          <a href="">
-            Acessar Repositórios
-          </a>
-        </li>
-        <li>
-          <strong>unfrom</strong>
-          <p>Froms in React</p>
-          <a href="">
-            Acessar Repositórios
-          </a>
-        </li>
-        <li>
-          <strong>unfrom</strong>
-          <p>Froms in React</p>
-          <a href="">
-            Acessar Repositórios
-          </a>
-        </li>
+      <ul>        
+      <RepositoryItem repository={repository}/>
+      <RepositoryItem repository={repository}/>     
+      <RepositoryItem repository={repository}/>
+      <RepositoryItem repository={repository}/>
       </ul>
     </section>
   )

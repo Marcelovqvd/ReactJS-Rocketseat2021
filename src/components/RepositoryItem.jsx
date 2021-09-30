93b39b1a20e20654107d9a26704091c87662b6a9
Marcelovqvd <marcelovqvd@gmail.com>
2021-09-30 15:02:18 -0300

    list repositories

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -2,10 +2,11 @@ export function RepositoryItem(props) {
   
   return (
     <li>
-      <strong>{props.repository?.name ?? 'Default'}</strong>
+      <strong>{props.repository.name}</strong>
       <p>{props.repository.description}</p>
-      <a href={props.repository.link}>
-        Acessar Repositórios
+
+      <a href={props.repository.html_url}>
+        Acessar repositório
       </a>
     </li>
   )
